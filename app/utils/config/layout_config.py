4b6700c466fcedfb8e05b58ff4a51d643f47b55c
SDG223157 <isky999@gmail.com>
2024-12-21 12:06:45 +0800

    Your commit message here

--- app/utils/config/layout_config.py
+++ app/utils/config/layout_config.py
@@ -34,10 +34,10 @@ LAYOUT_CONFIG = {
             'growth': {'x': 0.05, 'y': 0.16}
         },
         'stats': {
-            'price': {'x': 0.07, 'y': 0.92},
-            'regression': {'x': 0.30, 'y': 0.92},
-            'volatility': {'x': 0.53, 'y': 0.92},
-            'signals': {'x': 0.76, 'y': 0.92}
+            'price': {'x': 0.07, 'y': 1.00},
+            'regression': {'x': 0.30, 'y': 1.00},
+            'volatility': {'x': 0.53, 'y': 1.00},
+            'signals': {'x': 0.76, 'y': 1.00}
         }
     },
     'spacing': {
