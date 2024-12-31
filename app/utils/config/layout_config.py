1e1256003a7ce04c41fc7ddb18767b6224f7ce34
SDG223157 <isky999@gmail.com>
2024-12-31 08:25:13 +0800

    Your commit message here

--- app/utils/config/layout_config.py
+++ app/utils/config/layout_config.py
@@ -86,7 +86,8 @@ TABLE_STYLE = {
     'header': {
         'fill_color': 'lightgrey',
         'font': dict(size=12),
-        'align': 'left'
+        'align': 'left',
+        'height': 30
     },
     'cells': {
         'font': dict(size=11),
