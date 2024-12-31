698e44e1e7aedc4e157a022e7405334d199a2014
SDG223157 <isky999@gmail.com>
2024-12-31 08:18:29 +0800

    Your commit message here

--- app/utils/visualization/visualization_service.py
+++ app/utils/visualization/visualization_service.py
@@ -178,6 +178,7 @@ class VisualizationService:
                     ]
                 ],
                 **TABLE_STYLE['cells']
+                height=30  # Set fixed cell height
             )
         )
         
