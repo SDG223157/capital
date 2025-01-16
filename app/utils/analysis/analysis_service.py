808f0ec124cab7bd48508cfe7f6f02928caef547
SDG223157 <isky999@gmail.com>
2025-01-16 12:01:41 +0800

    Your commit message here

--- app/utils/analysis/analysis_service.py
+++ app/utils/analysis/analysis_service.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import math
+import random
 from datetime import datetime, timedelta
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
