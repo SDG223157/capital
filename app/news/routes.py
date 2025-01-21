05093e6139b4c7ad170bdff8bbc1d83f4a927401
SDG223157 <isky999@gmail.com>
2025-01-21 11:05:34 +0800

    Your commit message here

--- app/news/routes.py
+++ app/news/routes.py
@@ -25,7 +25,7 @@ def fetch_news(symbol):
         # days = request.args.get('days', default=5, type=int)
 
         news_service = NewsAnalysisService()
-        symbols = list(symbol)
+        symbols = [symbol]
         try:
             # end_date = datetime.now()
             # start_date = end_date - timedelta(days=days)
