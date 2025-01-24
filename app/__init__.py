8d9e762a2fe3a27aa8a237296d9c0e2ff48eba2e
SDG223157 <isky999@gmail.com>
2025-01-24 10:39:28 +0800

    Your commit message here

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)  # Link Flask-Migrate with the app and db
    
     login_manager.init_app(app)
-    from app.models import NewsArticle, ArticleMetric, ArticleSymbol, User  # Import models after db is initialized
+    # from app.models import NewsArticle, ArticleMetric, ArticleSymbol, User  # Import models after db is initialized
    
 
     # Force HTTPS
@@ -57,7 +57,7 @@ def create_app(config_class=Config):
             logger.info("Database tables created successfully")
 
             logger.info("Database initialized using Flask-Migrate")
-            # from app.models import NewsArticle, ArticleMetric, ArticleSymbol, User  # Import models after db is initialized
+            from app.models import NewsArticle, ArticleMetric, ArticleSymbol, User  # Import models after db is initialized
             # db.create_all()
 
             # Check if admin user exists, if not create one
