3b4ef430cc28a53bca1bbe5f281bb6106e9384a4
SDG223157 <80448068+SDG223157@users.noreply.github.com>
2025-02-23 15:02:37 +0800

    Update metrics_config.py

--- app/utils/config/metrics_config.py
+++ app/utils/config/metrics_config.py
@@ -5,8 +5,8 @@ METRICS_MAP = {
     "total revenues": "is_sales_and_services_revenues",
     "net income": "is_net_income",
     "earnings per share": "eps",
+    "operating income": "is_oper_income",
     "operating margin": "oper_margin",
-    "operating cash flow": "cf_cash_from_oper",
     "capital expenditures": "cf_cap_expenditures",
     "return on invested capital": "return_on_inv_capital",
     "diluted shares": "is_sh_for_diluted_eps"  # Updated metric name
@@ -17,7 +17,7 @@ METRICS_TO_FETCH = list(METRICS_MAP.keys())
 # Metrics that should include CAGR calculation
 CAGR_METRICS = {
     "total revenues",
-    "operating cash flow",
+    "operating income",
     "net income",
     "earnings per share"
 }
